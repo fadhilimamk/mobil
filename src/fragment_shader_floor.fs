973200ba68b17310ae9a7fcaf3969e390318dc7b
agent-whisper <fariz.tumbuan@gmail.com>
2018-05-16 11:31:51 +0700

    Added lighting to floor texture

--- src/fragment_shader_floor.fs
+++ src/fragment_shader_floor.fs
@@ -1,11 +1,30 @@
 #version 330 core
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 in vec3 floorColors;
 in vec2 texCords;
+in vec3 FragPos;
 
 out vec4 FragColor;
 
 uniform sampler2D ourTexture;
+uniform Light light;
 
 void main() {
-    FragColor = texture(ourTexture, texCords) * vec4(floorColors, 1.0f);
+	vec3 Normal = vec3(0, 1, 0);
+
+    vec3 ambient = light.ambient * texture(ourTexture, texCords).rgb;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * texture(ourTexture, texCords).rgb;
+
+    FragColor = vec4((ambient + diffuse), 1.0f);
 }
--- src/vertex_shader_floor.vs
+++ src/vertex_shader_floor.vs
@@ -4,6 +4,7 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 colors;
 layout (location = 2) in vec2 aTexCords;
 
+out vec3 FragPos;
 out vec3 floorColors;
 out vec2 texCords;
 
@@ -12,6 +13,7 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
+	FragPos = vec3(model * vec4(aPos, 1.0));
 	gl_Position = projection * view * model * vec4(aPos, 1.0f);
 	floorColors = colors;
 	texCords = aTexCords;
