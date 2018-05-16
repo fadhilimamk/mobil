de223290f977e795ce32e74d67ab60eac201fb54
agent-whisper <fariz.tumbuan@gmail.com>
2018-05-16 09:12:02 +0700

    Added floor shaders

---
+++ src/fragment_shader_floor.fs
@@ -0,0 +1,12 @@
+#version 330 core
+
+in vec3 floorColors;
+in vec2 texCords;
+
+out vec4 FragColor;
+
+uniform sampler2D ourTexture;
+
+void main() {
+    FragColor = texture(ourTexture, texCords) * vec4(floorColors, 1.0f);
+}
